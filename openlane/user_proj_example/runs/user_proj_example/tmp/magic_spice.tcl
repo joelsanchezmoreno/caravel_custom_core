dc12078e87e96ef8b3eef9ae4f04ece0c212b0e3
joel.sanchez.moreno <joel.sanchez.moreno@estudiantat.upc.edu>
2020-11-30 15:43:57 +0100

    Example project pass

--- openlane/user_proj_example/runs/user_proj_example/tmp/magic_spice.tcl
+++ openlane/user_proj_example/runs/user_proj_example/tmp/magic_spice.tcl
@@ -1,5 +1,5 @@
 
-lef read /home/ubuntu/google-shuttle/.install/pdks/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
+lef read /home/ubuntu/google-shuttle/new-env/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
 if {  [info exist ::env(EXTRA_LEFS)] } {
 	set lefs_in $::env(EXTRA_LEFS)
 	foreach lef_file $lefs_in {
